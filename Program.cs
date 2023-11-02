fea1238c4e53a92fd2685ff2ea02b77ce0456ebe
m0tr1x <grigorysyzran@mail.ru>
2023-11-02 18:40:37 +0300

    Made an repository

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ class Book
 abstract class User
 {
     // Поле для хранения роли пользователя
-    protected string Role { get; }
+    public string Role { get; }
 
     // ФИО пользователя
     private string _name;
@@ -162,8 +162,125 @@ static class Biblioteque
         example.Condition -= 1;
     }
 }
+/// <summary>
+/// Класс для сохранения и загрузки пользователей и книг
+/// </summary>
+class Repository
+{
+    /// <summary>
+    /// Метод для загрузки пользователей
+    /// </summary>
+    /// <param name="filePath">Путь к файлу</param>
+    /// <returns></returns>
+    public List<User> LoadUsers(string filePath)
+    {
+        List<User> users = new List<User>();
 
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
 
+            foreach (string line in lines)
+            {
+                string[] userData = line.Split(',');
+
+                string name = userData[0];
+                string role = userData[1];
+
+                if (role == "Читатель")
+                {
+                    users.Add(new Customer(name));
+                }
+                else if (role == "Работник")
+                {
+                    users.Add(new Librarian(name));
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ошибка чтения файла: {e.Message}");
+        }
+
+        return users;
+    }
+    /// <summary>
+    /// Метод для загрузки списка книг из файлов
+    /// </summary>
+    /// <param name="filePath">Путь к файлу</param>
+    /// <returns></returns>
+    public List<Book> LoadBooks(string filePath)
+    {
+        List<Book> books = new List<Book>();
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] bookData = line.Split(',');
+
+                string name = bookData[0];
+                string author = bookData[1];
+                double condition = double.Parse(bookData[2]);
+                bool available = bool.Parse(bookData[3]);
+
+                books.Add(new Book(name, author, condition, available));
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ошибка чтения файла: {e.Message}");
+        }
+
+        return books;
+    }
+    /// <summary>
+    /// Метод для сохранения пользователей в файл
+    /// </summary>
+    /// <param name="users"> Список пользователей</param>
+    /// <param name="filePath">Путь к файлу</param>
+    public void SaveUsers(List<User> users, string filePath)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (User user in users)
+                {
+                    writer.WriteLine($"{user.Name},{user.Role}");
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ошибка сохранения файла: {e.Message}");
+        }
+    }
+    /// <summary>
+    /// Метод для сохранения списка книг в файл
+    /// </summary>
+    /// <param name="books">Список книг</param>
+    /// <param name="filePath">Путь к файлу</param>
+    public void SaveBooks(List<Book> books, string filePath)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (Book book in books)
+                {
+                    writer.WriteLine($"{book.Name},{book.Author},{book.Condition},{book.Avaliable}");
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Ошибка сохранения файла: {e.Message}");
+        }
+    }
+}
 
 
 class Program
