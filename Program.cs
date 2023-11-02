110ca1c3d5693f3fd1723b7c32982a04f11bf390
m0tr1x <grigorysyzran@mail.ru>
2023-11-02 18:00:00 +0300

    Made and book class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,50 @@
-﻿class Program
+﻿/// <summary>
+/// Класс книги 
+/// </summary>
+class Book
+{
+    private string _name;
+    private string _author;
+    private double _condition;
+    // Название книги
+    public string Name 
+    {
+        get { return _name; }
+        set { if (value != null) _name = value; }
+    }
+    // Автор книги
+    public string Author 
+    {
+        get { return _author; }
+        set { if (value != null) _author = value; }
+    }
+
+    // Состояние книги
+    public double Condition
+    {
+        get { return _condition; }
+        set
+        {
+            if (value > 0 && value <= 100) _condition = value;
+        }
+    }
+    // Наличие книги
+    public bool Avaliable { get; set; }
+
+    public Book(string Name, string Author, double Condition, bool Avaliable)
+    {
+        this.Name = Name;
+        this.Author = Author;
+        this.Condition = Condition;
+        this.Avaliable = Avaliable;
+    }
+}
+
+
+
+
+
+class Program
 {
     static void Main()
     {
