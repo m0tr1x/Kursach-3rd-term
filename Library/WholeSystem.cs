0f92b77a97f568e854080e0230d8ec98d7bf24b2
m0tr1x <grigorysyzran@mail.ru>
2023-11-18 11:02:07 +0300

    Improved main

---
+++ Library/WholeSystem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Biblioteque
+{
+	public class WholeSystem
+	{
+        private bool programRunning = true;
+        private User currentUser = null;
+
+        /// <summary>
+        /// Метод для запуска логики
+        /// </summary>
+        public void Run()
+        {
+            DataHandler.LoadData();
+            AppDomain.CurrentDomain.ProcessExit += DataHandler.OnExit; //Подписываемся на событие выхода
+
+            Console.WriteLine("Добро пожаловать в библиотеку");
+
+            while (programRunning)
+            {
+                if (currentUser == null)
+                {
+                    HandleGuestActions();
+                }
+                else if (!Program.flag)
+                {
+                    ActionsHandler.UserActions(currentUser);
+                }
+                else
+                {
+                    currentUser = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Обрабатываем действия неавторизованного гостя
+        /// </summary>
+        private void HandleGuestActions()
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Войти");
+            Console.WriteLine("2. Зарегистрироваться");
+            Console.WriteLine("3. Выйти из программы");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    currentUser = AuthHandler.Login();
+                    if (currentUser != null)
+                    {
+                        ActionsHandler.UserActions(currentUser);
+                    }
+                    break;
+                case "2":
+                    AuthHandler.Register();
+                    break;
+                case "3":
+                    Console.WriteLine("До свидания!");
+                    programRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор. Пожалуйста, выберите снова.");
+                    break;
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -5,57 +5,12 @@ namespace Biblioteque
     class Program
     {
         public static bool flag;
-        static void Main()
+        public static void Main()
         {
-            DataHandler.LoadData(); //Загружаем данные при старте программы
-            AppDomain.CurrentDomain.ProcessExit += DataHandler.OnExit; // Подисываемся на событие закрытия приложения
-            Console.WriteLine("Добро пожаловать в библиотеку");
-            User currentUser = null;
-            //Смотрим логнился ли пользователь
-            
-                while (true)
-                {
-                if (currentUser == null)
-                {
-                    Console.WriteLine("Выберите действие:");
-                    Console.WriteLine("1. Войти");
-                    Console.WriteLine("2. Зарегистрироваться");
-                    Console.WriteLine("3. Выйти из программы");
-
-                    string choice = Console.ReadLine();//Выбор действия
-                                                       //Перебираем варианты
-                    switch (choice)
-                    {
-                        case "1":
-                            //Логинимся
-                            currentUser = AuthHandler.Login();
-                            if (currentUser == null) break; //Если логин прошел
-                            else //То выполняем действия для пользователя
-                            {
-                                ActionsHandler.UserActions(currentUser);
-                            }
-                            break;
-                        //Регистрация
-                        case "2":
-                            AuthHandler.Register();
-                            break;
-                        //Выход
-                        case "3":
-                            Console.WriteLine("До свидания!");
-                            return;
-                        default:
-                            Console.WriteLine("Некорректный выбор. Пожалуйста, выберите снова.");
-                            break;
-                    }
-                }
-                // Пользователь залогинен, выполняем его действия
-                else if (!flag) ActionsHandler.UserActions(currentUser);
-                else currentUser = null;
-
-
-            }
-
+            WholeSystem wholeSystem = new WholeSystem();
+            wholeSystem.Run();
         }
+        
     }
 }
 
